b4cc081456b42afa2153d23ef7223a3cbb2c2c89
wzj <weizhangjian@hypergryph.com>
2025-02-19 17:37:38 +0800

    after esc press, auto press f3, to keep tpv

---
+++ ExternalTool/ExternalToolMain.py
@@ -0,0 +1,14 @@
+import keyboard
+import time
+
+def on_esc_press():
+    # 当检测到 Esc 键按下时，延迟 0.1 秒
+    time.sleep(0.1)
+    # 然后模拟按下 F3 键
+    keyboard.press_and_release('f3')
+
+# 监听 Esc 键按下事件
+keyboard.on_press_key('esc', lambda _: on_esc_press())
+
+# 确保程序持续运行以监听按键
+keyboard.wait()
