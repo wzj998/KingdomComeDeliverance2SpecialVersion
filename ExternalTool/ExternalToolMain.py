945e4a0299d3e4d547675b6e99628e0b2c8ab4c5
wzj <weizhangjian@hypergryph.com>
2025-02-19 17:48:03 +0800

    0.2秒比较保险

--- ExternalTool/ExternalToolMain.py
+++ ExternalTool/ExternalToolMain.py
@@ -2,8 +2,8 @@ import keyboard
 import time
 
 def on_esc_press():
-    # 当检测到 Esc 键按下时，延迟 0.1 秒
-    time.sleep(0.1)
+    # 当检测到 Esc 键按下时，延迟 0.2 秒
+    time.sleep(0.2)
     # 然后模拟按下 F3 键
     keyboard.press_and_release('f3')
 
