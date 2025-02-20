9d60c983017713142ac68c4e51c25f6b48b3aed8
wzj <weizhangjian@hypergryph.com>
2025-02-20 14:12:13 +0800

    打开地图后取消自动奔跑

--- ExternalTool/ExternalToolMain.py
+++ ExternalTool/ExternalToolMain.py
@@ -1,7 +1,20 @@
 import keyboard
 import time
 
-caps_lock_state = False
+b_auto_run = False
+
+def set_b_auto_run(value_new):
+    if value_new:
+        keyboard.press('w')
+        keyboard.press('shift')
+        print("按住 w+shift")
+    else:
+        keyboard.release('w')
+        keyboard.release('shift')
+        print("释放 w+shift")
+        
+    global b_auto_run
+    b_auto_run = value_new
 
 def on_esc_press():
     # 当检测到 Esc 键按下时，延迟 0.2 秒
@@ -11,22 +24,22 @@ def on_esc_press():
     print("按下 f3")
 
 def on_caps_lock_press_event(e):
-    global caps_lock_state
-    if not caps_lock_state:
-        # 首次按下时，模拟按住 w 和 shift
-        keyboard.press('w')
-        keyboard.press('shift')
-        caps_lock_state = True
-        print("按住 w+shift")
-    else:
-        # 再次按下时，释放 w 和 shift
-        keyboard.release('w')
-        keyboard.release('shift')
-        caps_lock_state = False
-        print("释放 w+shift")
+    global b_auto_run
+    set_b_auto_run(not b_auto_run)
+
+def on_m_press():
+    global b_auto_run
+    if b_auto_run:
+        set_b_auto_run(False)
+
+        time.sleep(0.1)
+
+        keyboard.press_and_release('m')
+        print("按下 m")
 
 keyboard.on_press_key('caps lock', on_caps_lock_press_event)
 keyboard.on_press_key('esc', lambda _: on_esc_press())
+keyboard.on_press_key('m', lambda _: on_m_press())
 
 # 确保程序持续运行以监听按键
 keyboard.wait()
