a3554118d38b1bd5f4dd0ca675e16bec064daae3
wzj <weizhangjian@hypergryph.com>
2025-02-26 12:30:15 +0800

    改成keyup

--- ExternalTool/ExternalToolMain.py
+++ ExternalTool/ExternalToolMain.py
@@ -16,18 +16,18 @@ def set_b_auto_run(value_new):
     global b_auto_run
     b_auto_run = value_new
 
-def on_esc_press():
+def on_esc_release():
     # 当检测到 Esc 键按下时，延迟 0.2 秒
     time.sleep(0.2)
     # 然后模拟按下 F3 键
     keyboard.press_and_release('f3')
     print("按下 f3")
 
-def on_caps_lock_press_event(e):
+def on_caps_lock_release_event(e):
     global b_auto_run
     set_b_auto_run(not b_auto_run)
 
-def on_m_press():
+def on_m_release():
     global b_auto_run
     if b_auto_run:
         set_b_auto_run(False)
@@ -37,9 +37,9 @@ def on_m_press():
         keyboard.press_and_release('m')
         print("按下 m")
 
-keyboard.on_press_key('caps lock', on_caps_lock_press_event)
-keyboard.on_press_key('esc', lambda _: on_esc_press())
-keyboard.on_press_key('m', lambda _: on_m_press())
+keyboard.on_release_key('caps lock', on_caps_lock_release_event)
+keyboard.on_release_key('esc', lambda _: on_esc_release())
+keyboard.on_release_key('m', lambda _: on_m_release())
 
 # 确保程序持续运行以监听按键
 keyboard.wait()
